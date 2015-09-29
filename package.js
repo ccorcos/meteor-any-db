b98bfcd2de669ed3984de3f9919f698c933a271a
Chet Corcos <ccorcos@gmail.com>
2015-09-28 21:48:17 -0700

    imply

--- package.js
+++ package.js
@@ -7,19 +7,19 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2');
-
-  api.use([
+  var packages = [
     'coffeescript',
     'random',
     'id-map',
     'diff-sequence',
     'meteorhacks:unblock@1.1.0',
     'ccorcos:utils@0.0.1'
-  ]);
-
+  ];
+  api.use(packages);
+  api.imply(packages);
   api.addFiles(['globals.js']);
   api.addFiles(['pub.coffee'], 'server');
   api.addFiles(['sub.coffee'], 'client');
-  
+
   api.export(['AnyDb']);
 });
